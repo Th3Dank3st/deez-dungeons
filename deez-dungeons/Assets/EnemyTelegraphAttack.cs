1f8c2be0a3228c0fc124ae512a09c9240110f6ad
Th3Dank3st <max.beisch1@gmail.com>
2022-05-04 09:49:37 -0500

    added Warlock and added Warlocks first attack

---
+++ deez-dungeons/Assets/EnemyTelegraphAttack.cs
@@ -0,0 +1,79 @@
+        using System.Collections;
+        using System.Collections.Generic;
+        using UnityEngine;
+
+public class EnemyTelegraphAttack : MonoBehaviour
+{
+    //public Transform firePoint;
+    //public GameObject bulletPrefab;
+    public float FireRate = 6.0f;
+    public float NextFire = 1.0f;
+    public float bulletForce;
+    private bool playerInRange;
+    public Animator m_animator;
+    private bool Attacking;
+    private Transform player;
+    Vector2 playerPos;
+    Vector2 playerLastPos;
+    public GameObject Tele1;
+    public GameObject Attack1;
+    private GameObject oldTele;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            if (Time.time > NextFire)
+            {
+                NextFire = Time.time + FireRate;
+                {
+                    StartCoroutine(Attack());
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+            player = other.gameObject.transform;
+            playerPos = new Vector2(player.position.x, player.position.y);
+            
+        }       
+    }
+
+    IEnumerator Attack()
+    {
+        float i = 1;
+        while (i > 0)
+        {
+            if(!Attacking)
+            {
+                playerPos = new Vector2(player.position.x, player.position.y);
+                Attacking = true;
+                //set animation trigger
+                m_animator.SetTrigger("Spellcast");
+                GameObject newTele = Instantiate(Tele1, playerPos, Quaternion.identity);
+                playerLastPos = playerPos;
+                oldTele = newTele;
+            }
+
+            yield return new WaitForSeconds(1f);
+            i--;
+        }
+        
+        Debug.Log("Attack Casted");
+        GameObject newAttack = Instantiate(Attack1, playerLastPos, Quaternion.identity);
+        Destroy(oldTele, 1f);
+        Destroy (newAttack, 1f);
+        Attacking = false;
+    }
+}
+
+
---
+++ deez-dungeons/Assets/WarlockAoeExplosion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarlockAoeExplosion : MonoBehaviour
+{
+    public float damage;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerMovement>().UpdateHealth(-damage);
+        }
+    }
+}
