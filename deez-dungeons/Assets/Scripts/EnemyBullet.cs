73a61f736666ef681ec2a05363cfc8716d6b5a33
Th3Dank3st <max.beisch1@gmail.com>
2022-05-05 21:22:59 -0500

    fixed issue with skulls

--- deez-dungeons/Assets/Scripts/EnemyBullet.cs
+++ deez-dungeons/Assets/Scripts/EnemyBullet.cs
@@ -15,7 +15,7 @@ public class EnemyBullet : MonoBehaviour
     }
 
     
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
 
         if (other.gameObject.tag == "Player") 
