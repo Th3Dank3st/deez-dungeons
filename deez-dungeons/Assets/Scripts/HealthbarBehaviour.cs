65095673b4fa8e6ca8cbe5d2c15853e9d3fc6b9e
Th3Dank3st <max.beisch1@gmail.com>
2022-05-07 14:29:11 -0500

    enemy health bar v1.0

--- deez-dungeons/Assets/Scripts/HealthbarBehaviour.cs
+++ deez-dungeons/Assets/Scripts/HealthbarBehaviour.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class HealthbarBehaviour : MonoBehaviour
 {
     public Slider Slider;
-    public GameObject slider;
     public Color Low;
     public Color High;
-    public Vector2 Offset;
+    public Vector3 Offset;
     public GameObject enemy;
-    private float duration = 50000;
+    private Vector3 enemypos;
+    private float duration = .1f;
+
 
     public void SetHealth(float health, float maxHealth)
     {
@@ -20,23 +21,22 @@ public class HealthbarBehaviour : MonoBehaviour
         Slider.maxValue = maxHealth;
         Slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(Low, High, Slider.normalizedValue);
     }
-
     private void Awake()
+    {
+        //StartCoroutine(SliderFollower());
+    }
+    private void FixedUpdate()
     {
         StartCoroutine(SliderFollower());
     }
-    // void FixedUpdate()
-    // {
-
-    // }
-
     IEnumerator SliderFollower()
     {
-        Vector2 enemypos = enemy.transform.position;
+        enemypos = enemy.transform.position;
         float i = 0;
         while (i < duration)
         {
-            slider.transform.position = Vector2.Lerp(slider.transform.position, enemypos + Offset, i / duration);
+            
+            Slider.transform.position = Vector3.Lerp(Slider.transform.position, enemypos + Offset, i / duration);
             i += Time.deltaTime;
             yield return null;
         }
