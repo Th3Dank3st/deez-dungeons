bc9d7d78bd0829a03aee21e609eb693cfb55a402
Th3Dank3st <max.beisch1@gmail.com>
2022-05-10 03:18:14 -0500

    0.2

--- deez-dungeons/Assets/Scripts/ItemPickup.cs
+++ deez-dungeons/Assets/Scripts/ItemPickup.cs
@@ -11,6 +11,7 @@ public class ItemPickup : MonoBehaviour
     private bool stat2 = false;
     private bool stat3 = false;
     private bool stat4 = false;
+    private bool stat5 = false;
 
     public void OnCollisionEnter2D(Collision2D other)      // the original line of code for this function is Pickup(); only
     {
@@ -27,24 +28,36 @@ public class ItemPickup : MonoBehaviour
     }
 
     void Pickup()
-    {   
-        //MagicBoots
-       if (itemz.itemType == Item.ItemType.Boots)
+    {
+        //redpot
+        if (itemz.itemType == Item.ItemType.Potion)
         {
-            itemz.speed = Random.Range(1, 20);
-            itemz.defense = Random.Range(3, 5);
-            StartCoroutine(StatSelector(1));
-           
+            InventoryManager.Instance.Add(itemz);
+            Destroy(gameObject);
+        }
+        //MagicBoots
+        if (itemz.itemType == Item.ItemType.Boots)
+        {
+            itemz.speed = Random.Range(0, 20);
+            itemz.defense = Random.Range(0, 5);
+            StartCoroutine(StatSelector(2));
         }
 
         //MagicArmor
-        if(itemz.itemType == Item.ItemType.Armor)
+        if (itemz.itemType == Item.ItemType.Armor)
         {
             itemz.defense = Random.Range(1, 20);
-            StartCoroutine(StatSelector(3));           
+            StartCoroutine(StatSelector(2));
         }
-    }
 
+        //MagicStaff
+        if (itemz.itemType == Item.ItemType.Staff)
+        {
+            itemz.attackDamage = Random.Range(1, 20);
+            StartCoroutine(StatSelectorStaff(2));
+        }
+
+    }
 
 
     IEnumerator StatSelector(int numberOfStats)
@@ -101,6 +114,65 @@ public class ItemPickup : MonoBehaviour
         itemz.manaRegen = 0;
         itemz.maxHealth = 0;
         itemz.healthRegen = 0;
+        Destroy(gameObject);
+    }
+
+    IEnumerator StatSelectorStaff(int numberOfStats)
+    {
+
+        while (numberOfStats > 0)
+        {
+            int statSelector1 = Random.Range(1, 6);
+            if (statSelector1 == 1 && !stat1)
+            {
+                itemz.attackSpeed = Random.Range(1, 20);
+                numberOfStats--;
+                stat1 = true;
+            }
+
+            if (statSelector1 == 2 && !stat2)
+            {
+                itemz.spellDamage = Random.Range(1, 20);
+                numberOfStats--;
+                stat2 = true;
+            }
+
+            if (statSelector1 == 3 && !stat3)
+            {
+                itemz.maxMana = Random.Range(1, 20);
+                numberOfStats--;
+                stat3 = true;
+            }
+
+            if (statSelector1 == 4 && !stat4)
+            {
+                itemz.manaRegen = Random.Range(1, 20);
+                numberOfStats--;
+                stat4 = true;
+            }
+
+            if (statSelector1 == 5 && !stat5)
+            {
+                itemz.castSpeed = Random.Range(1, 20);
+                numberOfStats--;
+                stat5 = true;
+            }
+            yield return null;
+        }
+        Debug.Log(itemz.castSpeed + " cast spd");
+        Debug.Log(itemz.attackDamage + " attack dmg");
+        Debug.Log(itemz.attackSpeed + " attack Spd");
+        Debug.Log(itemz.spellDamage + " spell Damage");
+        Debug.Log(itemz.maxMana + " mana");
+        Debug.Log(itemz.manaRegen + " manaregen");
+
+        InventoryManager.Instance.Add(itemz);
+        itemz.attackDamage = 0;
+        itemz.attackSpeed = 0;
+        itemz.spellDamage = 0;
+        itemz.castSpeed = 0;
+        itemz.maxMana = 0;
+        itemz.manaRegen = 0;
         Destroy(gameObject);
     }
 }
