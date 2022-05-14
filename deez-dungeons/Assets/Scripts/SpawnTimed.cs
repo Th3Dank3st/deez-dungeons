4538e27c19865d83247b13d89c90e25ede3a1ee3
Th3Dank3st <max.beisch1@gmail.com>
2022-05-14 10:32:54 -0500

    dd

--- deez-dungeons/Assets/Scripts/SpawnTimed.cs
+++ deez-dungeons/Assets/Scripts/SpawnTimed.cs
@@ -17,46 +17,23 @@ public class SpawnTimed : MonoBehaviour
     public Transform spawner;
     public GameObject spawnerobject;
     public float spawnRange = 5f;
-
     public float spawnerActiveTime;
-
-
-
-
-
-    
-
 
     Vector3 GeneratedPosition()
     {
-
         return (Vector2)spawner.position + spawnRange * Random.insideUnitCircle;
     }
 
-    //    public void Start()
-    //{
-            //private float swarmerInterval = (spawnerActiveTime) / (swarmer - 0.01f);
-            //private float eliteInterval = (spawnerActiveTime) / (elite - 0.05f);
-
-    //}
-private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        //float swarmers = (spawnerActiveTime) / (swarmer - 0.02f);
-        //float elites = (spawnerActiveTime) / (elite - 0.3f);
-        
-
         if (other.gameObject.tag == "Player")
-        {
-            {
-                //float swarmerInterval = (spawnerActiveTime) / (swarmer - 0.02f);
-                //float eliteInterval = (spawnerActiveTime) / (elite - 0.05f);
-                StartCoroutine(spawnEnemy(swarmerInterval, swarmerPrefab));
-                StartCoroutine(spawnEnemy(eliteInterval, bigSwarmerPrefab));
-                Destroy(spawnerobject, spawnerActiveTime);
-            }
-            
+        {            
+                if (spawnerActiveTime > 0)
+                {
+                    StartCoroutine(spawnEnemy(swarmerInterval, swarmerPrefab));
+                    StartCoroutine(spawnEnemy(eliteInterval, bigSwarmerPrefab));
+                }
         }
-        
     }
 
     public IEnumerator spawnEnemy(float interval, GameObject enemy)
@@ -64,6 +41,8 @@ private void OnTriggerEnter2D(Collider2D other)
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, GeneratedPosition(), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
+        spawnerActiveTime -= interval;
+
         //add counter to make endless
 
     }
